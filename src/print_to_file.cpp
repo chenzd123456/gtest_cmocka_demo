8d7bef288ca3dc27374e5eff46facd0ef28d7db3
adam <chenzd123456@gmail.com>
2025-02-10 12:14:05 +0800

    修复 print_to_file 函数中的 mock 函数调用，改为使用标准文件操作函数

--- src/print_to_file.cpp
+++ src/print_to_file.cpp
@@ -5,18 +5,18 @@
 #include "../test/mock_functions.h"
 
 int print_to_file(const char *filename, const char *text) {
-    int fd = mock_open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
     if (fd == -1) {
         return -1;
     }
 
-    ssize_t bytes_written = mock_write(fd, text, strlen(text));
+    ssize_t bytes_written = write(fd, text, strlen(text));
     if (bytes_written == -1) {
-        mock_close(fd);
+        close(fd);
         return -1;
     }
 
-    if (mock_close(fd) == -1) {
+    if (close(fd) == -1) {
         return -1;
     }
 
